af36c0b3bdc7f28f1e7085fdb8920ae60dfe7b5c
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-07-11 11:27:16 -0300

    feat: adicao do dto do usuario

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/domain/dto/UpdateUserDto.java
@@ -0,0 +1,18 @@
+package br.fai.lds.lifebank.domain.dto;
+
+import br.fai.lds.lifebank.domain.UserModel;
+import lombok.Getter;
+import lombok.Setter;
+
+public class UpdateUserDto {
+
+    private int id;
+    private String fullName;
+
+    public UserModel toUserModel() {
+        UserModel entity = new UserModel();
+        entity.setId(id);
+        entity.setFullname(fullName);
+        return entity;
+    }
+}
