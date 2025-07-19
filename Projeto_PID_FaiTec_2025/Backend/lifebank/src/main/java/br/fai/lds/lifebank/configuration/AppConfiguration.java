5422af32138e021777256085649c572eec75ce61
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-07-19 10:39:11 -0300

    feat: adicao do bean da doacao

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/configuration/AppConfiguration.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/configuration/AppConfiguration.java
@@ -1,8 +1,10 @@
 package br.fai.lds.lifebank.configuration;
 
 import br.fai.lds.lifebank.implementation.dao.fake.BloodFakeDaoImpl;
+import br.fai.lds.lifebank.implementation.dao.fake.DonationFakeDaoImpl;
 import br.fai.lds.lifebank.implementation.dao.fake.UserFakeDaoImpl;
 import br.fai.lds.lifebank.port.dao.blood.BloodDao;
+import br.fai.lds.lifebank.port.dao.donation.DonationDao;
 import br.fai.lds.lifebank.port.dao.user.UserDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,5 +33,10 @@ public class AppConfiguration {
     @Bean
     public BloodDao getBloodFakeDao() {
         return new BloodFakeDaoImpl();
+    }
+
+    @Bean
+    public DonationDao getDonationFakeDao() {
+        return new DonationFakeDaoImpl();
     }
 }
