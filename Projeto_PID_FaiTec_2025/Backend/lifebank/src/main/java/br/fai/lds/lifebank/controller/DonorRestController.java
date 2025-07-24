a90d696fe0e0d0ab175b7df7ab1af2e1fa0cd088
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-07-24 15:02:56 -0300

    feat: adicao do controller do doador

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/controller/DonorRestController.java
@@ -0,0 +1,75 @@
+package br.fai.lds.lifebank.controller;
+
+import br.fai.lds.lifebank.domain.DonorModel;
+import br.fai.lds.lifebank.port.service.donor.DonorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/donor")
+public class DonorRestController {
+
+    private final DonorService donorService;
+
+    public DonorRestController(DonorService donorService) {
+        this.donorService = donorService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<DonorModel>> getEntities() {
+        List<DonorModel> entities = donorService.findAll();
+
+        return ResponseEntity.ok(entities);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DonorModel> getEntityById(@PathVariable final int id) {
+        DonorModel entity = donorService.findByid(id);
+
+        return entity == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(entity);
+    }
+
+    @GetMapping("/find-by-cpf")
+    public ResponseEntity<DonorModel> getEntityByCpf(@RequestBody final String cpf) {
+        DonorModel entity = donorService.findByCpf(cpf);
+
+        return entity == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable final int id) {
+        donorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity<Boolean> deleteByCpf(@PathVariable final String cpf) {
+        donorService.deleteByCpf(cpf);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<DonorModel> create(@RequestBody final DonorModel data) {
+        final int id = donorService.create(data);
+
+        final URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/`{id}")
+    public ResponseEntity<DonorModel> update(@PathVariable final int id, @RequestBody final DonorModel data) {
+
+        DonorModel entity = data;
+        donorService.update(id, entity);
+
+        return ResponseEntity.noContent().build();
+    }
+}
