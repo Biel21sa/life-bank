36df12e5ce0dca9208d64b66d7c699464fc3c76f
AndreySFaria <andreyeeddm@gmail.com>
2025-08-15 19:12:08 -0300

    feat: Basicos do sistema de sidenav-clinica

--- Projeto_PID_FaiTec_2025/Frontend/life-bank/src/app/components/account/main/main.ts
+++ Projeto_PID_FaiTec_2025/Frontend/life-bank/src/app/components/account/main/main.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Component, computed, signal, viewChild } from '@angular/core';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +12,9 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import * as fontawesome from '@fortawesome/free-solid-svg-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+
+import { phosphorHouseSimpleBold } from '@ng-icons/phosphor-icons/bold'
 
 @Component({
   selector: 'app-main',
@@ -28,10 +31,30 @@ import * as fontawesome from '@fortawesome/free-solid-svg-icons';
     MatTooltipModule,
     FontAwesomeModule,
     RouterModule,
+    NgIcon,
+    MatListModule,
+  ],
+  providers: [
+    provideIcons({phosphorHouseSimpleBold})
   ],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
+
 export class Main {
   FontAwesome = fontawesome
+
+  activate(button: string){
+    console.log(button + " activated.");
+  }
+
+
+  sidenavWidth = '250px'
+  collapse(){
+    if (this.sidenavWidth == '250px'){
+      this.sidenavWidth = '70px'
+    }else{
+      this.sidenavWidth = '250px'
+    }
+  }
 }
