fa61abb965744acb68024116c161249e4344220a
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-08-26 20:54:18 -0300

    feat: criacao da autenticacao

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/controller/AuthenticationRestController.java
@@ -0,0 +1,35 @@
+package br.fai.lds.lifebank.controller;
+
+import br.fai.lds.lifebank.domain.UserModel;
+import br.fai.lds.lifebank.domain.dto.AuthenticationDto;
+import br.fai.lds.lifebank.port.service.authentication.AuthenticationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authenticate")
+public class AuthenticationRestController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationRestController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserModel> authenticate(@RequestBody final AuthenticationDto authenticationDto) {
+
+        UserModel authenticatedUser = authenticationService.authenticate(
+                authenticationDto.getEmail(),
+                authenticationDto.getPassword());
+
+        if(authenticatedUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(authenticatedUser);
+    }
+}
