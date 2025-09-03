5aa062e2bf4d68bd15b0b261d5a2e4f34770bc9d
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-03 20:50:25 -0300

    feat: criacao do BenefitPostgresDaoImpl

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/dao/postgres/BenefitPostgresDaoImpl.java
@@ -0,0 +1,49 @@
+package br.fai.lds.lifebank.implementation.dao.postgres;
+
+import br.fai.lds.lifebank.domain.BenefitModel;
+import br.fai.lds.lifebank.port.dao.benefit.BenefitDao;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class BenefitPostgresDaoImpl implements BenefitDao {
+
+    private static final Logger logger = Logger.getLogger(BenefitPostgresDaoImpl.class.getName());
+
+    private final Connection connection;
+
+    public BenefitPostgresDaoImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public List<BenefitModel> findByClinicId(int clinicId) {
+        return List.of();
+    }
+
+    @Override
+    public int create(BenefitModel entity) {
+        return 0;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public BenefitModel findByid(int id) {
+        return null;
+    }
+
+    @Override
+    public List<BenefitModel> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void update(int id, BenefitModel entity) {
+
+    }
+}
