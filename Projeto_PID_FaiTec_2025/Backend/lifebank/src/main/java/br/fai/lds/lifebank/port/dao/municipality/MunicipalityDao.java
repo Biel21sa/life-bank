0f6fe3d3dd6fbb1b3fb3c34810c02858057991fc
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-04 15:29:44 -0300

    feat: adicao do dao e model de municipio

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/port/dao/municipality/MunicipalityDao.java
@@ -0,0 +1,7 @@
+package br.fai.lds.lifebank.port.dao.municipality;
+
+import br.fai.lds.lifebank.domain.MunicipalityModel;
+import br.fai.lds.lifebank.port.dao.crud.ReadDao;
+
+public interface MunicipalityDao extends ReadDao<MunicipalityModel> {
+}
