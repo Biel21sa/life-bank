470c6965d5ed513ef56c4164c132cf6b82180ad6
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-04 15:30:33 -0300

    feat: adicao do controller do estoque de sangue

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/controller/BloodStockRestController.java
@@ -0,0 +1,9 @@
+package br.fai.lds.lifebank.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/blood-stock")
+public class BloodStockRestController {
+}
