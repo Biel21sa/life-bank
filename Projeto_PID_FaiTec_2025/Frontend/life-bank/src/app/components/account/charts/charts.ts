eeabcd6b7027da19518778cade6e1493e2d0e19c
AndreySFaria <andreyeeddm@gmail.com>
2025-09-04 17:02:47 -0300

    feat: adicao do chart.js

--- Projeto_PID_FaiTec_2025/Frontend/life-bank/src/app/components/account/charts/charts.ts
+++ Projeto_PID_FaiTec_2025/Frontend/life-bank/src/app/components/account/charts/charts.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { Home } from "../home/home";
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables} from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-charts',
-  imports: [Home],
+  imports: [],
   templateUrl: './charts.html',
   styleUrl: './charts.css'
 })
-export class Charts {
+export class Charts implements OnInit {
 
+  public config: any = {
+    type: 'bar',
+    data: {
+      labels: ['1','2','3','4'],
+      datasets:[
+        {label: 'teste',
+          data: ['100', '12', '144', '200'],
+        },
+        {label: 'teste2',
+          data: ['10', '50', '13', '2'],
+        }
+      ],
+    },
+    options: {
+      apectRatio: 1,
+    }
+  }
+  chart: any
+
+  ngOnInit(){
+    this.chart = new Chart('Nome', this.config)
+  }
 }
--- Projeto_PID_FaiTec_2025/Frontend/life-bank/src/app/components/login/login.ts
+++ Projeto_PID_FaiTec_2025/Frontend/life-bank/src/app/components/login/login.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { UserCredentialDto } from '../../domain/dto/user-credential-dto';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements OnInit {
 
+  form: FormGroup
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  ngOnInit(): void {
+    console.log(environment.api_endpoint);
+    
+  }
+
+  validateFields(){
+    return this.form.controls['email'].valid && this.form.controls['password'].valid
+  }
+
+  login() {
+    console.log('botao de login clicado');
+
+    let credentials: UserCredentialDto = {
+      email: this.form.controls['email'].value!,
+      password: this.form.controls['password'].value!,
+    };
+
+    console.log(credentials);
+  }
 }
