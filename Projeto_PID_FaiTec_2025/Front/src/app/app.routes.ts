9b2c0ff20d31d9256d4d1d6201ee5a50f898e255
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-08 13:47:58 -0300

    feat: adiconado as rotas de criacao, listagem, edicao, detalhes, da clinica e doador

--- Projeto_PID_FaiTec_2025/Front/src/app/app.routes.ts
+++ Projeto_PID_FaiTec_2025/Front/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './views/account/sign-in/sign-in.component';
-
 import { MyProfileComponent } from './views/account/my-profile/my-profile.component';
 import { MainComponent } from './views/app/main/main.component';
 import { HomeComponent } from './views/app/home/home.component';
@@ -19,6 +18,16 @@ import { RoleHomeComponent } from './views/app/role-home/role-home.component';
 import { UserHomeComponent } from './views/app/user-home/user-home.component';
 import { ClinicHomeComponent } from './views/app/clinic-home/clinic-home.component';
 import { AdminHomeComponent } from './views/app/admin-home/admin-home.component';
+import { AdministratorGuard } from './guards/administrator.guard';
+import { DonorListComponent } from './views/app/donor/donor-list/donor-list.component';
+import { DonorCreateComponent } from './views/app/donor/donor-create/donor-create.component';
+import { DonorEditComponent } from './views/app/donor/donor-edit/donor-edit.component';
+import { DonorDetailComponent } from './views/app/donor/donor-detail/donor-detail.component';
+import { ClinicListComponent } from './views/app/clinic/clinic-list/clinic-list.component';
+import { ClinicCreateComponent } from './views/app/clinic/clinic-create/clinic-create.component';
+import { ClinicEditComponent } from './views/app/clinic/clinic-edit/clinic-edit.component';
+import { ClinicDetailComponent } from './views/app/clinic/clinic-detail/clinic-detail.component';
+import { DonationCreateComponent } from './views/app/donation-create/donation-create.component';
 
 export const routes: Routes = [
     {
@@ -81,6 +90,55 @@ export const routes: Routes = [
                         component: UserCreateComponent
                     },
                 ]
+            },
+            {
+                path: 'donor',
+                canActivate: [AdministratorGuard],
+                children: [
+                    {
+                        path: 'list',
+                        component: DonorListComponent
+                    },
+                    {
+                        path: 'create',
+                        component: DonorCreateComponent
+                    },
+                    {
+                        path: 'edit/:id',
+                        component: DonorEditComponent
+                    },
+                    {
+                        path: 'detail/:id',
+                        component: DonorDetailComponent
+                    }
+                ]
+            },
+            {
+                path: 'clinic',
+                canActivate: [AdministratorGuard],
+                children: [
+                    {
+                        path: 'list',
+                        component: ClinicListComponent
+                    },
+                    {
+                        path: 'create',
+                        component: ClinicCreateComponent
+                    },
+                    {
+                        path: 'edit/:id',
+                        component: ClinicEditComponent
+                    },
+                    {
+                        path: 'detail/:id',
+                        component: ClinicDetailComponent
+                    }
+                ]
+            },
+            {
+                path: 'donation-create',
+                component: DonationCreateComponent,
+                canActivate: [AdministratorGuard]
             },
             {
                 path: 'donation-location',
