efd96f72534f2e91055a5c8016a5970875563a30
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-08 12:12:32 -0300

    refactor: ajustes nas interfaces do doador

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/port/service/donor/DonorService.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/port/service/donor/DonorService.java
@@ -3,5 +3,5 @@ package br.fai.lds.lifebank.port.service.donor;
 import br.fai.lds.lifebank.domain.DonorModel;
 import br.fai.lds.lifebank.port.service.crud.CrudService;
 
-public interface DonorService extends CrudService<DonorModel>, DeleteByCpfService, ReadByCpfService {
+public interface DonorService extends CrudService<DonorModel> {
 }
