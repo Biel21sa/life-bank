fae8a7dc31efedd6d6c09a27cf55a3283c58f758
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-08 13:34:18 -0300

    feat: criado novos campos para beneficio

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/domain/BenefitModel.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/domain/BenefitModel.java
@@ -5,19 +5,23 @@ import java.time.LocalDate;
 public class BenefitModel {
     private int id;
     private String description;
-    private Double value;
-    private LocalDate validity;
-    private int clinicId;
+    private Double amount;
+    private LocalDate expirationDate;
+    private int donationId;
+    private int donorId;
+    private DonationModel donation;
+    private DonorModel donor;
 
     public BenefitModel() {
     }
 
-    public BenefitModel(int id, String description, Double value, LocalDate validity, int clinicId) {
+    public BenefitModel(int id, String description, Double amount, LocalDate expirationDate, int donationId, int donorId) {
         this.id = id;
         this.description = description;
-        this.value = value;
-        this.validity = validity;
-        this.clinicId = clinicId;
+        this.amount = amount;
+        this.expirationDate = expirationDate;
+        this.donationId = donationId;
+        this.donorId = donorId;
     }
 
     public int getId() {
@@ -36,28 +40,52 @@ public class BenefitModel {
         this.description = description;
     }
 
-    public Double getValue() {
-        return value;
+    public Double getAmount() {
+        return amount;
     }
 
-    public void setValue(Double value) {
-        this.value = value;
+    public void setAmount(Double amount) {
+        this.amount = amount;
     }
 
-    public LocalDate getValidity() {
-        return validity;
+    public LocalDate getExpirationDate() {
+        return expirationDate;
     }
 
-    public void setValidity(LocalDate validity) {
-        this.validity = validity;
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
     }
 
-    public int getClinicId() {
-        return clinicId;
+    public int getDonationId() {
+        return donationId;
     }
 
-    public void setClinicId(int clinicId) {
-        this.clinicId = clinicId;
+    public void setDonationId(int donationId) {
+        this.donationId = donationId;
+    }
+
+    public int getDonorId() {
+        return donorId;
+    }
+
+    public void setDonorId(int donorId) {
+        this.donorId = donorId;
+    }
+
+    public DonationModel getDonation() {
+        return donation;
+    }
+
+    public void setDonation(DonationModel donation) {
+        this.donation = donation;
+    }
+
+    public DonorModel getDonor() {
+        return donor;
+    }
+
+    public void setDonor(DonorModel donor) {
+        this.donor = donor;
     }
 }
 
