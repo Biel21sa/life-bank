f6f7915b61ba40f88a80349ca719ad167b163d03
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-08 13:38:52 -0300

    feat: criado o modelo donation

---
+++ Projeto_PID_FaiTec_2025/Front/src/app/domain/model/donation.ts
@@ -0,0 +1,17 @@
+import { Blood } from "./blood";
+import { DonationLocation } from "./donation-location";
+import { Donor } from "./donor";
+
+export interface Donation {
+    id?: string;
+    bloodType: string;
+    quantity: number;
+    collectionDate: Date;
+    expirationDate: Date;
+    donorId: string;
+    donationLocationId: string;
+    bloodId?: number;
+    donor?: Donor;
+    blood?: Blood;
+    donationLocation?: DonationLocation;
+}
