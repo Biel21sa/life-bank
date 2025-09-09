6d0d460f1d1513559803e200f71b1c3e58a778e0
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-09 14:32:35 -0300

    feat: adicao do service de read e update do sangue

---
+++ Projeto_PID_FaiTec_2025/Front/src/app/services/blood/blood-update.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface BloodWithdrawalRequest {
+  bloodIds: string[];
+  reason: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BloodUpdateService {
+
+  constructor(private http: HttpClient) { }
+
+  withdrawBlood(request: BloodWithdrawalRequest): Observable<any> {
+    return this.http.put(`${environment.api_endpoint}/blood/withdraw`, request);
+  }
+}
