38c07b548446283fdd2c7ff0db86a4bc4d24ea4c
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-11 15:57:48 -0300

    feat: adicao do botao para a rota de verificacao do beneficio do doador

--- Projeto_PID_FaiTec_2025/Front/src/app/views/app/main/main.component.ts
+++ Projeto_PID_FaiTec_2025/Front/src/app/views/app/main/main.component.ts
@@ -60,6 +60,10 @@ export class MainComponent {
     return this.authenticationService.isUser();
   }
 
+  isClinic(): boolean {
+    return this.authenticationService.isClinic();
+  }
+
   public logout() {
     console.log('logout clicado');
     this.authenticationService.logout();
