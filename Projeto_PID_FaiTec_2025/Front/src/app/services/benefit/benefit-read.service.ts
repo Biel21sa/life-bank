33bd8b47e4a4a4c4da4b74c3c83a15ed2a366429
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-11 11:21:24 -0300

    feat: adicao do service read de beneficio

---
+++ Projeto_PID_FaiTec_2025/Front/src/app/services/benefit/benefit-read.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Benefit } from '../../domain/model/benefit';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BenefitReadService {
+
+  constructor(private http: HttpClient) { }
+
+  getBenefitsByUserId(userId: string): Observable<Benefit[]> {
+    return this.http.get<Benefit[]>(`${environment.api_endpoint}/benefit/user/${userId}`);
+  }
+}
