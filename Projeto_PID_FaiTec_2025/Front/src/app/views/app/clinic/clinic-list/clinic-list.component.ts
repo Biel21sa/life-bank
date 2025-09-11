a57264023a59f735e4d7e8ed4e98fef157583361
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-11 11:20:14 -0300

    feat: adicao da visualizacao da listagem das clinicas pelo usuario doador

--- Projeto_PID_FaiTec_2025/Front/src/app/views/app/clinic/clinic-list/clinic-list.component.ts
+++ Projeto_PID_FaiTec_2025/Front/src/app/views/app/clinic/clinic-list/clinic-list.component.ts
@@ -8,13 +8,13 @@ import { ToastrService } from 'ngx-toastr';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { NgxMaskPipe } from 'ngx-mask';
-import { MatTableDataSource } from '@angular/material/table';
+import { AuthenticationService } from '../../../../services/security/authentication.service';
 
 @Component({
   selector: 'app-clinic-list',
@@ -35,7 +35,7 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrl: './clinic-list.component.css'
 })
 export class ClinicListComponent implements OnInit {
-  
+
   clinics: User[] = [];
   dataSource = new MatTableDataSource(this.clinics);
   displayedColumns: string[] = ['name', 'clinic', 'contact', 'address', 'actions'];
@@ -44,15 +44,16 @@ export class ClinicListComponent implements OnInit {
     private userReadService: UserReadService,
     private userDeleteService: UserDeleteService,
     private toastr: ToastrService,
-  ){}
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
     this.loadClinics();
   }
 
-  async loadClinics(){
+  async loadClinics() {
     let clinicList = await this.userReadService.findByRole(UserRole.CLINIC);
-    if(clinicList == null) {
+    if (clinicList == null) {
       return;
     }
 
@@ -63,6 +64,10 @@ export class ClinicListComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  isAdministrator(): boolean {
+    return this.authenticationService.isAdministrator();
   }
 
   async deleteClinic(clinicId: string) {
