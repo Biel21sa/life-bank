fe7ee53c9d6629e9b49699c2a8e119a622b6a2e5
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-23 11:20:32 -0300

    feat: adicao de um design aprimorado na tela clinic-home

--- Projeto_PID_FaiTec_2025/Front/src/app/views/app/clinic-home/clinic-home.component.ts
+++ Projeto_PID_FaiTec_2025/Front/src/app/views/app/clinic-home/clinic-home.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { UserReadService } from '../../../services/user/user-read.service';
+import { User } from '../../../domain/model/user';
+import { AuthenticationService } from '../../../services/security/authentication.service';
 
 @Component({
   selector: 'app-clinic-home',
@@ -11,14 +14,34 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     MatCardModule,
     MatButtonModule,
+    RouterModule,
     MatIconModule
   ],
   templateUrl: './clinic-home.component.html',
   styleUrls: ['./clinic-home.component.css']
 })
-export class ClinicHomeComponent {
+export class ClinicHomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userReadService: UserReadService, private authenticationService: AuthenticationService) { }
+
+  user: User | null = null;
+
+  ngOnInit() {
+    const userId = this.authenticationService.getUserId?.();
+    if (userId) {
+      this.getUserInfo(userId);
+    }
+  }
+
+  getUserInfo(userId: string) {
+    this.userReadService.findById(userId)
+      .then((user: any) => {
+        this.user = user || null;
+      })
+      .catch(() => {
+        this.user = null;
+      });
+  }
 
   navigateToBenefits() {
     this.router.navigate(['/benefit/verification']);
