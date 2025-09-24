48372f5e1ebc03cac1f0c8b7e05d208a9ee36f86
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-23 22:44:03 -0300

    feat: adicao do controller message

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/controller/MessagesRestController.java
@@ -0,0 +1,9 @@
+package br.fai.lds.lifebank.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesRestController {
+}
