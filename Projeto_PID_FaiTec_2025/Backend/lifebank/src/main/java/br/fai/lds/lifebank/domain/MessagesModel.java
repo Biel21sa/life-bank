9a5881168d4449230e5228b584a0ab7f51b1bf35
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-09-23 22:43:31 -0300

    feat: adicao do model message

---
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/domain/MessagesModel.java
@@ -0,0 +1,63 @@
+package br.fai.lds.lifebank.domain;
+
+import java.time.LocalDate;
+
+public class MessagesModel {
+
+    private int id;
+    private int senderId;
+    private int receiverId;
+    private String message;
+    private LocalDate sentAt;
+
+    public MessagesModel() {
+    }
+
+    public MessagesModel(int id, int senderId, int receiverId, String message, LocalDate sentAt) {
+        this.id = id;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.message = message;
+        this.sentAt = sentAt;
+    }
+
+    public LocalDate getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDate sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(int receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
