355a33e8f531efd53e556c14682eb54cb9dc7d9d
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-10-31 15:08:09 -0300

    feat: implementacao do conteudov base do JwtService

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/JwtService.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/JwtService.java
@@ -1,4 +1,50 @@
 package br.fai.lds.lifebank.implementation.service.authentication.jwt;
 
+import io.jsonwebtoken.Claims;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Profile("jwt")
+@Component
 public class JwtService {
+
+    private final String secret = "XUFAE3FQG1RLBlgQ93fDSUlj4HfbKi4a1kFl1gDloOg=";
+
+    public String getEmailFromToken(String valor){
+        return "";
+    }
+
+    public Date getExpirationDateFromTOken(String token){
+        return null;
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsTFunction){
+        return null;
+    }
+
+    public Claims getAllClaimsFromToken(String token){
+        return null;
+    }
+
+    public boolean tokenExpired(String token){
+        return false;
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        return false;
+    }
+
+    public String generateToken(String Token, UserDetails userDetails){
+        return "";
+    }
+
+    private String createToken(Map<String, Object> claims, String subjct){
+        return "";
+    }
+
 }
