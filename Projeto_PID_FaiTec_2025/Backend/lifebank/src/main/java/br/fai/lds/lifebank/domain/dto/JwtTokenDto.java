4ec48430f32f0a2e99082f75608705a05d6afd16
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-10-31 12:18:08 -0300

    feat: adicao da variavel token do jwt no JwtTokenDto

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/domain/dto/JwtTokenDto.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/domain/dto/JwtTokenDto.java
@@ -1,4 +1,17 @@
 package br.fai.lds.lifebank.domain.dto;
 
 public class JwtTokenDto {
+    private String token;
+
+    public JwtTokenDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
