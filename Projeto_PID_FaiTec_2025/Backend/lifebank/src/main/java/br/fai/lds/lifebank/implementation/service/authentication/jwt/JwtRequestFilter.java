f4f16764efb0b567cd804f3abe1c67c3b7d21fc3
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-10-31 15:11:39 -0300

    feat: finalizacao da implementacao do JwtRequestFilter

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/JwtRequestFilter.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/JwtRequestFilter.java
@@ -1,11 +1,16 @@
 package br.fai.lds.lifebank.implementation.service.authentication.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,6 +30,31 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+        String email = null;
+        String jwtToken = null;
 
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")){
+            try {
+                email = jwtService.getEmailFromToken(jwtToken);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Nao foi possivel obter o token!");
+            } catch (ExpiredJwtException e){
+                System.out.println("O token expirou!");
+            }
+        } else {
+            System.out.println("O token recebido nao iniciou com a string Bearer");
+        }
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(email);
+            if (jwtService.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
