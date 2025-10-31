249488696a1e04392663b6f04e0452215dd20492
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-10-31 15:08:42 -0300

    feat: implementacao do conteudov base do JwtRequestFilter

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/JwtRequestFilter.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/JwtRequestFilter.java
@@ -1,4 +1,30 @@
 package br.fai.lds.lifebank.implementation.service.authentication.jwt;
 
-public class JwtRequestFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Profile("jwt")
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtRequestFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
