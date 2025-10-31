3a8a282309b60d03cf8de9741748b5037946d407
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2025-10-31 15:07:25 -0300

    feat: implementacao do CustomUserDetailsService

--- Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/CustomUserDetailsService.java
+++ Projeto_PID_FaiTec_2025/Backend/lifebank/src/main/java/br/fai/lds/lifebank/implementation/service/authentication/jwt/CustomUserDetailsService.java
@@ -1,4 +1,38 @@
 package br.fai.lds.lifebank.implementation.service.authentication.jwt;
 
-public class CustomUserDetailsService {
+import br.fai.lds.lifebank.domain.UserModel;
+import br.fai.lds.lifebank.port.service.user.UserService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Profile("jwt")
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
+        final UserModel userModel = userService.findByEmail(email);
+
+        if (userModel == null){
+            throw new UsernameNotFoundException("Email nao encontrado");
+        }
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(userModel.getRole().name()));
+
+        return new User(userModel.getEmail(), userModel.getPassword(), authorities);
+    }
 }
